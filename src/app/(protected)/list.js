8bfc888dbb755b2616c5c8a215bece1d89d53a70
Arthuruivo486 <160732432+Arthuruivo486@users.noreply.github.com>
2024-11-25 21:32:47 +0000

    [feat] Payment list

--- src/app/(protected)/list.js
+++ src/app/(protected)/list.js
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { usePaymentsDatabase } from "../../database/usePaymentsDatabase";
+import { FlashList } from "@shopify/flash-list";
 
 export default function List() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Lista</Text>
+  const [data, setData] = useState([]);
+  const { getPayments } = usePaymentsDatabase();
+
+  async function fetchData() {
+
+    const payments = await getPayments(); // Execute a função corretamente aqui
+    setData(payments)
+  }
+
+  useEffect(() => {
+
+    fetchData(); // Chame a função para buscar os dados na inicialização
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={{ flexDirection: "row", margin: 5 }}>
+      <View style={{ flex: 1 }}>
+        <Text>{item.nome}</Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+          <Text>{item.data_pagamento}</Text>
+          <Text>{item.numero_recibo}</Text>
+        </View>
       </View>
-    );
+      <View>
+        <Text>{item.valor_pago}</Text>
+      </View>
+    </View>
+  );
+  
+
+  return (
+    <View style={{ flex: 1}}>
+      <Text>Pagamentos</Text>
+        <View style={{ flex: 1}}>
+          <FlashList
+          data={data}
+          renderItem={renderItem}
+         estimatedItemSize={200}
+
+        />
+        </View>
+    </View>
+  );
 }
--- src/app/(protected)/payment.js
+++ src/app/(protected)/payment.js
@@ -8,6 +8,7 @@ import { z } from "zod";
 import { useUsersDatabase } from '../../database/useUsersDatabase';
 import { useAuth } from "../../hooks/Auth/index";
 import { usePaymentsDatabase } from "../../database/usePaymentsDatabase";
+import { Alert } from "react-native";
 
 
 
@@ -26,49 +27,16 @@ const paymentScheema = z.object({
   data_pagamento: z.date(),
   numero_recibo: z.string(),
   observacao: z.string().optional(),
-
-})
+});
 
 
 export default function Payment() {
 
   const [valor, setValor] = useState("0,00");
   const [sugestoes, setSugestoes] = useState([
-    { "id": 1, "nome": "Lance Astlett" },
-    { "id": 2, "nome": "Toby Pandie" },
-    { "id": 3, "nome": "Mariam Ormes" },
-    { "id": 4, "nome": "Lenci Emanulsson" },
-    { "id": 5, "nome": "Jdavie Levick" },
-    { "id": 6, "nome": "Lyn Coulman" },
-    { "id": 7, "nome": "Culver Binding" },
-    { "id": 8, "nome": "Marla Hardes" },
-    { "id": 9, "nome": "Jodi Mourbey" },
-    { "id": 10, "nome": "Lenore Noriega" },
-    { "id": 11, "nome": "Gian Waudby" },
-    { "id": 12, "nome": "Pammy Douce" },
-    { "id": 13, "nome": "Rourke Rathjen" },
-    { "id": 14, "nome": "Griselda Muckley" },
-    { "id": 15, "nome": "Mary Rillatt" },
-    { "id": 16, "nome": "Bambie Mardle" },
-    { "id": 17, "nome": "Jamison Noton" },
-    { "id": 18, "nome": "Lyn Brimson" },
-    { "id": 19, "nome": "Barnaby Moberley" },
-    { "id": 20, "nome": "Georgetta Brodley" },
-    { "id": 21, "nome": "Murielle Figiovanni" },
-    { "id": 22, "nome": "Hurlee Frizzell" },
-    { "id": 23, "nome": "Dory Denk" },
-    { "id": 24, "nome": "Ricard Gaskell" },
-    { "id": 25, "nome": "Fanchette Audritt" },
-    { "id": 26, "nome": "Benoite Cranny" },
-    { "id": 27, "nome": "Niki Gutans" },
-    { "id": 28, "nome": "Gill Jaram" },
-    { "id": 29, "nome": "Martica Cardiff" },
-    { "id": 30, "nome": "Sheilakathryn Cudbird" },
-    { "id": 31, "nome": "Susy Andryushchenko" },
-    { "id": 32, "nome": "Haily Samett" },
-    { "id": 33, "nome": "Celestine Blackden" },
-    { "id": 34, "nome": "Ibrahim Perrott" },
-    { "id": 35, "nome": "Rosita Grint" }
+    { "id": 1, "nome": "Adim" },
+    { "id": 2, "nome": "Super" },
+    { "id": 3, "nome": "User" },
   ]);
 
   const [id, setId] = useState(1);
@@ -139,23 +107,31 @@ const handleSubmit = async () => {
   const payment = {
     user_id: id,
     user_cadastro: Number(user.user.id),
-    valor_pago: convertValue(valor),
-    data_pagamento: data,
+    valor_pago: convertValue(valor), // Certifique-se de que este retorna um número
+    data_pagamento: data, // Certifique-se de que seja uma instância de Date
     numero_recibo: numeroRecibo,
     observacao,
   };
 
+  console.log("Dados enviados para validação:", payment); // Adicione este log
+
   try {
     const result = await paymentScheema.parseAsync(payment);
+    console.log("Dados validados com sucesso:", result); // Log para dados validados
     const { insertedID } = await createPayment(payment);
-    console.log("Dados validados com sucesso:", result);
-    console.log(insertedID)
+    console.log("ID inserido:", insertedID);
     setNumeroRecibo("");
   } catch (error) {
-    Alert.alert("Erro", `Erro de validação: ${error.mensage}`)
-    console.log(error);
+    if (error instanceof z.ZodError) {
+      console.log("Erros de validação:", error.errors); // Log dos erros de validação
+      Alert.alert("Erro", `Erro de validação: ${error.errors.map(e => e.message).join(", ")}`);
+    } else {
+      console.log("Erro inesperado:", error); // Log de outros erros
+      Alert.alert("Erro", "Ocorreu um erro inesperado.");
+    }
   }
 };
+
 
 return (
 
